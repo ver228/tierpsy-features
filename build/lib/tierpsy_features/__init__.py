05487f819dfc54318948aa734e2cb986bb1401cd
ver228 <ajaver@MRC-8791.local>
2017-10-10 19:09:51 +0100

    reorganization

--- build/lib/tierpsy_features/__init__.py
+++
@@ -1,5 +0,0 @@
-# # -*- coding: utf-8 -*-
-import os
-
-EIGEN_PROJECTION_FILE = os.path.join(os.path.dirname(__file__), 'extras', 'master_eigen_worms_N2.mat')
-assert os.path.exists(EIGEN_PROJECTION_FILE)
