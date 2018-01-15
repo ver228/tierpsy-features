d135dc7a4e6e02af44e819d90c8322312a2e5193
Unknown <ver228@gmail.com>
2018-01-15 18:33:49 +0000

    .

--- experiments/classify/scripts/classify_SWDB.py
+++ experiments/classify/scripts/classify_SWDB.py
@@ -77,7 +77,8 @@ def _h_cross_validate(args):
 
 if __name__ == '__main__':
     
-    save_dir = '/Users/ajaver/OneDrive - Imperial College London/classify_strains/manual_features/SWDB/'
+    #save_dir = '/Users/ajaver/OneDrive - Imperial College London/classify_strains/manual_features/SWDB/'
+    save_dir = '../data/SWDB'
     feat_files = {
             'OW_old' : 'ow_features_old_SWDB.csv',
             'OW' : 'ow_features_SWDB.csv',
@@ -103,157 +104,187 @@ if __name__ == '__main__':
     
     
     #%%
-    cross_validation_fold = 5
-    n_trials = 200
+    from sklearn.model_selection import StratifiedShuffleSplit
+    from sklearn.metrics import f1_score
+    n_estimators = 200
     
-    n_batch= mp.cpu_count()
-    p = mp.Pool(n_batch)
-    
-    id_index_str = 'strain_id'
-    
-    cross_val_results = {}
-    
-    start = time.time()
+    results = {}
     for db_name, feats in feat_data.items():
         print(db_name)
-        
-        
         col_feats = [x for x in feats.columns if x not in col2ignore_r]
         
-        n_samples = feats['strain'].value_counts().min()
-        args = feats, col_feats, id_index_str, n_samples, cross_validation_fold, 1000
-        
-        #func = partial(_h_cross_validate, )
-        scores = list(p.map(_h_cross_validate, n_trials*[args]))
-        scores = np.concatenate(scores)
-        #scores = _h_cross_validate(feats_r, col_feats, id_index_str, n_samples, cross_validation_fold)
-    
-        print("%s Accuracy: %0.2f (+/- %0.2f)" % (db_name, scores.mean(), scores.std() * 2))    
-        cross_val_results[db_name] = scores
+        y = feats['strain_id'].values
+        X = feats[col_feats].values
         
         
-        print(time.time() - start)
-    
-    
-    #%%
-    if False:
-        n_batch= mp.cpu_count()
-        p = mp.Pool(n_batch)
-        
-        id_index_str = 'strain_base_id'
-        
-        cross_val_results = {}
-        
-        
-        for db_name, feats in feat_data.items():
-            print(db_name)
+        res = []
+        sss = StratifiedShuffleSplit(n_splits = 10, test_size = 0.2, random_state=777)
+        for ii, (train_index, test_index) in enumerate(sss.split(X, y)):
             
-            col_feats = [x for x in feats.columns if x not in col2ignore_r]
-            good = ~feats['strain_base_id'].isnull() & (feats['set_type'] == 'train')
-            feats_r = feats[good]
+            x_train, y_train  = X[train_index], y[train_index]
+            x_test, y_test  = X[test_index], y[test_index]
             
-            n_samples = feats_r['strain'].value_counts().min()
+            clf = RandomForestClassifier(n_estimators = n_estimators)
+            clf.fit(x_train, y_train)
             
+            y_pred = clf.predict(x_test)
             
-            selected_feats = []
+            f1 = f1_score(y_test, y_pred, average='weighted')
             
-            for n_feat in range(30):
-                n_trials = 20
-                
-                all_scores = []
-                for iif, ff in enumerate(col_feats):
-                    if ff in selected_feats:
-                        continue
-                    
-                    start = time.time()
-                    c_feats = selected_feats + [ff]
-                    
-                    args = feats_r, c_feats, id_index_str, n_samples, cross_validation_fold, 150
-                    #func = partial(_h_cross_validate, )
-                    scores = list(p.map(_h_cross_validate, n_trials*[args]))
-                    scores = np.concatenate(scores)
-                    #scores = _h_cross_validate(feats_r, col_feats, id_index_str, n_samples, cross_validation_fold)
-                    
-                    print(len(c_feats), iif+1, len(col_feats),  c_feats)
-                    print("%s Accuracy: %0.2f (+/- %0.2f)" % (db_name, scores.mean(), scores.std() * 2)) 
-                    print(scores.min(), scores.max(), scores.mean() - 2*scores.std())
-                    all_scores.append((c_feats, scores))
-                    
-                    print(time.time() - start)
-                
-                selected_feats = max(all_scores, key=lambda x : x[1].mean() - 2*x[1].std())[0]
-                with open('/Users/ajaver/OneDrive - Imperial College London/classify_strains/manual_features/SWDB/best_feats_OW.txt', 'a+') as fid:
-                    fid.write(', '.join(selected_feats) + '\n')
-    #%%
-    selected_feats = ['midbody_crawling_amplitude_abs', 'foraging_amplitude_abs', 'hips_bend_mean_pos', 'midbody_width_forward', 'neck_bend_sd_forward_abs', 'midbody_speed_pos', 'foraging_speed_pos', 'eigen_projection_4_forward_neg', 'bend_count_backward', 'tail_crawling_frequency_pos', 'length_forward', 'tail_bend_mean_forward_neg', 'head_bend_sd_forward_pos', 'midbody_bend_sd', 'head_bend_sd_neg', 'tail_tip_motion_direction_backward_pos', 'eigen_projection_3_paused_abs', 'head_tip_speed_paused_pos', 'eigen_projection_1_forward_neg', 'eigen_projection_5_forward_pos', 'head_tip_speed_forward', 'tail_speed_forward_neg', 'tail_bend_mean_forward', 'upsilon_turns_time_pos', 'path_range_paused', 'midbody_crawling_frequency', 'head_motion_direction_forward', 'width_length_ratio_backward', 'head_motion_direction_pos', 'path_curvature_paused']
-    
-    feats = feat_data['OW_old']
-    id_index_str = 'strain_base_id'
-    
-    n_batch= mp.cpu_count()
-    p = mp.Pool(n_batch)
-    
-    col_feats = [x for x in feats.columns if x not in col2ignore_r]
-    good = ~feats['strain_base_id'].isnull() & (feats['set_type'] == 'train')
-    feats_r = feats[good]
-    
-    n_samples = feats_r['strain'].value_counts().min()
-    
-    all_scores = []
-    for n_feats in range(len(selected_feats)):
-        start = time.time()
-        feat_cols = selected_feats[:n_feats+1]
+            print(ii, f1)
+            res.append((clf, f1))
         
-        print(n_feats, feat_cols)
-        n_trials = 200
-        args = feats_r, feat_cols, id_index_str, n_samples, cross_validation_fold, 1000
-        #func = partial(_h_cross_validate, )
-        scores = list(p.map(_h_cross_validate, n_trials*[args]))
-        scores = np.concatenate(scores)
-        #scores = _h_cross_validate(feats_r, col_feats, id_index_str, n_samples, cross_validation_fold)
-    
-        print("%s Accuracy: %0.2f (+/- %0.2f)" % (db_name, scores.mean(), scores.std() * 2))    
-        all_scores.append(scores)
-        print(time.time() - start) 
-    #%%
-    
-    yy = [x.mean() for x in all_scores]
-    err = [x.std() for x in all_scores]
-    
-    plt.figure()
-    plt.errorbar(np.arange(1, len(yy)+1), yy, yerr=err)
-    plt.ylabel('Accuracy')
-    plt.xlabel('Number of features')
-    plt.savefig('classification_accuracy.png')
-    
+        
+        results[db_name] = res
         
     #%%
-    '''
-    SWDB
-    Random Forest 1000 trees
-    200 trials 5-fold cross validation
-    
-    OW_old Accuracy: 0.57 (+/- 0.16)
-    OW Accuracy: 0.52 (+/- 0.16)
-    tierpsy Accuracy: 0.52 (+/- 0.17)
-    
-    
-    Accuracy with 30 feats
-    
-    '''
-    
-    '''
-    CeNDR
-    Random Forest 1000 trees
-    200 trials 5-fold cross validation
-    
-    OW Accuracy: 0.77 (+/- 0.13)
-    tierpsy Accuracy: 0.71 (+/- 0.13)
-    '''
-    '''
-    
-    
-    '''
+#    cross_validation_fold = 5
+#    n_trials = 200
+#    
+#    n_batch= mp.cpu_count()
+#    p = mp.Pool(n_batch)
+#    
+#    id_index_str = 'strain_id'
+#    
+#    cross_val_results = {}
+#    
+#    start = time.time()
+#    for db_name, feats in feat_data.items():
+#        print(db_name)
+#        
+#        
+#        col_feats = [x for x in feats.columns if x not in col2ignore_r]
+#        
+#        n_samples = feats['strain'].value_counts().min()
+#        args = feats, col_feats, id_index_str, n_samples, cross_validation_fold, 1000
+#        
+#        #func = partial(_h_cross_validate, )
+#        scores = list(p.map(_h_cross_validate, n_trials*[args]))
+#        scores = np.concatenate(scores)
+#        #scores = _h_cross_validate(feats_r, col_feats, id_index_str, n_samples, cross_validation_fold)
+#    
+#        print("%s Accuracy: %0.2f (+/- %0.2f)" % (db_name, scores.mean(), scores.std() * 2))    
+#        cross_val_results[db_name] = scores
+#        
+#        
+#        print(time.time() - start)
+#    
+#    
+#    #%%
+#    if False:
+#        n_batch= mp.cpu_count()
+#        p = mp.Pool(n_batch)
+#        
+#        id_index_str = 'strain_base_id'
+#        
+#        cross_val_results = {}
+#        
+#        
+#        for db_name, feats in feat_data.items():
+#            print(db_name)
+#            
+#            col_feats = [x for x in feats.columns if x not in col2ignore_r]
+#            good = ~feats['strain_base_id'].isnull() & (feats['set_type'] == 'train')
+#            feats_r = feats[good]
+#            
+#            n_samples = feats_r['strain'].value_counts().min()
+#            
+#            
+#            selected_feats = []
+#            
+#            for n_feat in range(30):
+#                n_trials = 20
+#                
+#                all_scores = []
+#                for iif, ff in enumerate(col_feats):
+#                    if ff in selected_feats:
+#                        continue
+#                    
+#                    start = time.time()
+#                    c_feats = selected_feats + [ff]
+#                    
+#                    args = feats_r, c_feats, id_index_str, n_samples, cross_validation_fold, 150
+#                    #func = partial(_h_cross_validate, )
+#                    scores = list(p.map(_h_cross_validate, n_trials*[args]))
+#                    scores = np.concatenate(scores)
+#                    #scores = _h_cross_validate(feats_r, col_feats, id_index_str, n_samples, cross_validation_fold)
+#                    
+#                    print(len(c_feats), iif+1, len(col_feats),  c_feats)
+#                    print("%s Accuracy: %0.2f (+/- %0.2f)" % (db_name, scores.mean(), scores.std() * 2)) 
+#                    print(scores.min(), scores.max(), scores.mean() - 2*scores.std())
+#                    all_scores.append((c_feats, scores))
+#                    
+#                    print(time.time() - start)
+#                
+#                selected_feats = max(all_scores, key=lambda x : x[1].mean() - 2*x[1].std())[0]
+#                with open('/Users/ajaver/OneDrive - Imperial College London/classify_strains/manual_features/SWDB/best_feats_OW.txt', 'a+') as fid:
+#                    fid.write(', '.join(selected_feats) + '\n')
+#    #%%
+#    selected_feats = ['midbody_crawling_amplitude_abs', 'foraging_amplitude_abs', 'hips_bend_mean_pos', 'midbody_width_forward', 'neck_bend_sd_forward_abs', 'midbody_speed_pos', 'foraging_speed_pos', 'eigen_projection_4_forward_neg', 'bend_count_backward', 'tail_crawling_frequency_pos', 'length_forward', 'tail_bend_mean_forward_neg', 'head_bend_sd_forward_pos', 'midbody_bend_sd', 'head_bend_sd_neg', 'tail_tip_motion_direction_backward_pos', 'eigen_projection_3_paused_abs', 'head_tip_speed_paused_pos', 'eigen_projection_1_forward_neg', 'eigen_projection_5_forward_pos', 'head_tip_speed_forward', 'tail_speed_forward_neg', 'tail_bend_mean_forward', 'upsilon_turns_time_pos', 'path_range_paused', 'midbody_crawling_frequency', 'head_motion_direction_forward', 'width_length_ratio_backward', 'head_motion_direction_pos', 'path_curvature_paused']
+#    
+#    feats = feat_data['OW_old']
+#    id_index_str = 'strain_base_id'
+#    
+#    n_batch= mp.cpu_count()
+#    p = mp.Pool(n_batch)
+#    
+#    col_feats = [x for x in feats.columns if x not in col2ignore_r]
+#    good = ~feats['strain_base_id'].isnull() & (feats['set_type'] == 'train')
+#    feats_r = feats[good]
+#    
+#    n_samples = feats_r['strain'].value_counts().min()
+#    
+#    all_scores = []
+#    for n_feats in range(len(selected_feats)):
+#        start = time.time()
+#        feat_cols = selected_feats[:n_feats+1]
+#        
+#        print(n_feats, feat_cols)
+#        n_trials = 200
+#        args = feats_r, feat_cols, id_index_str, n_samples, cross_validation_fold, 1000
+#        #func = partial(_h_cross_validate, )
+#        scores = list(p.map(_h_cross_validate, n_trials*[args]))
+#        scores = np.concatenate(scores)
+#        #scores = _h_cross_validate(feats_r, col_feats, id_index_str, n_samples, cross_validation_fold)
+#    
+#        print("%s Accuracy: %0.2f (+/- %0.2f)" % (db_name, scores.mean(), scores.std() * 2))    
+#        all_scores.append(scores)
+#        print(time.time() - start) 
+#    #%%
+#    
+#    yy = [x.mean() for x in all_scores]
+#    err = [x.std() for x in all_scores]
+#    
+#    plt.figure()
+#    plt.errorbar(np.arange(1, len(yy)+1), yy, yerr=err)
+#    plt.ylabel('Accuracy')
+#    plt.xlabel('Number of features')
+#    plt.savefig('classification_accuracy.png')
+#    
+#        
+#    #%%
+#    '''
+#    SWDB
+#    Random Forest 1000 trees
+#    200 trials 5-fold cross validation
+#    
+#    OW_old Accuracy: 0.57 (+/- 0.16)
+#    OW Accuracy: 0.52 (+/- 0.16)
+#    tierpsy Accuracy: 0.52 (+/- 0.17)
+#    
+#    
+#    Accuracy with 30 feats
+#    
+#    '''
+#    
+#    '''
+#    CeNDR
+#    Random Forest 1000 trees
+#    200 trials 5-fold cross validation
+#    
+#    OW Accuracy: 0.77 (+/- 0.13)
+#    tierpsy Accuracy: 0.71 (+/- 0.13)
+#    '''
     #%%
 #    id_index_str = 'strain_base_id'
 #    classifiers_d = {}
