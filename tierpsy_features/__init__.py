ea626ab51dbc5fe6048e126e7d15560ac2a24f3a
ver228 <ver228@gmail.com>
2018-04-12 12:15:45 +0100

    update

--- tierpsy_features/__init__.py
+++ tierpsy_features/__init__.py
@@ -1,9 +1,6 @@
 # # -*- coding: utf-8 -*-
 import os
 
-EIGEN_PROJECTION_FILE = os.path.join(os.path.dirname(__file__), 'extras', 'master_eigen_worms_N2.mat')
-assert os.path.exists(EIGEN_PROJECTION_FILE)
-
 from .velocities import get_velocity_features
 from .postures import get_morphology_features, get_posture_features
 from .smooth import SmoothedWorm
